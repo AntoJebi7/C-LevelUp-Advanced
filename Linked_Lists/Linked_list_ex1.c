e5612a66cc202f72de7c0c43a8048c7c5380ec99
Anto Jebikshan <dantojebi@gmail.com>
2025-01-11 11:45:38 +0530

    test

---
+++ Linked_Lists/Linked_list_ex1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+typedef struct {
+    int data;
+    struct node *next;
+}node;
+int printlist(node *n) {
+    while(n!=NULL) {
+        printf(" %d ->",n->data);
+        n = n->next ;
+    }
+    printf(" Null\n");
+    return 0;
+
+}
+int main() {
+    node n1 = {1,NULL};
+    node n2 = {2,NULL};
+    node n3 = {3,NULL};
+    n1.next = &n2;
+    n2.next=&n3;
+    printlist(&n1);
+
+    return 0;
+}
